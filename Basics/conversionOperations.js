f5121bba47a423d202b89c163eb6eab01b86b6a7
Arzoo Bhuker <118011789+Arzoo12222145@users.noreply.github.com>
2025-07-10 16:13:44 +0000

    Basic commit

--- Basics/conversionOperations.js
+++ Basics/conversionOperations.js
@@ -1,12 +1,38 @@
 // let str = "33"
 let str = "33abc"
-console.log(typeof(str));
+// console.log(typeof(str));
 
 let num = Number(str);
-console.log(typeof(num));
-console.log(num);
+// console.log(typeof(num));
+// console.log(num);
 
 // "33" -> 33
 // "33abc" -> NaN (type = number)
 // "arzoo" -> NaN (type=number)
 // true -> 1, false -> 0
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"2");
+// console.log("1"+2+3);
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let counter = 100;
+// counter++;
+// console.log(counter);
+
+
+console.log(2>=0);
+
+console.log("2">=0);
+console.log(null>=0);
+console.log(null==0);
+console.log(null>0);
+console.log(undefined>=0);
+console.log(undefined>0);
+console.log(undefined==0);
--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -1,8 +1,8 @@
-let age = 21
-let name = "arzoo"
-let check = false
-let state = undefined
-let node = null
+// let age = 21
+// let name = "arzoo"
+// let check = false
+// let state = undefined
+// let node = null
 
 // number -> 2 to power 53
 // string
@@ -10,5 +10,59 @@ let node = null
 // null -> standalone unit
 // undefined -> the variable hasn't yet been defined
 
-console.log(typeof state); // undefined
-console.log(typeof node); // object
+// console.log(typeof state); // undefined
+// console.log(typeof node); // object
+
+
+// Primitive data types (Call by value)
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt 
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+
+// const s1 = Symbol('123');
+// const s2 = Symbol('123');
+// console.log(s1);
+// console.log(s2);
+// console.log(s1==s2);
+
+
+// const heros = ["shaktiman","naagraj","harryPotter"];
+
+// const obj = {
+//     name: "Harry",
+//     age : 21,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello");
+// }
+// myFunction();
+
+// console.log(typeof(myFunction));
+// console.log(typeof(heros));
+
+
+//******************************************************************************** */
+
+//Stack(Primitive data types) | Heap(Non Primitive data types)
+
+let myname = "arzoo"
+
+let anothername = myname
+anothername = "bhuker"
+
+console.log(myname);
+console.log(anothername);
+
+
+let userOne = {
+    name : "arzuu",
+    email: "abc@ybl"
+}
+
+let user = userOne.email;
+console.log(user);
