70d7fc96108b4af3832a87cd597a4db22654c109
Arzoo Bhuker <118011789+Arzoo12222145@users.noreply.github.com>
2025-07-10 14:26:32 +0000

    Basic Commit

---
+++ Basics/variable.js
@@ -0,0 +1,10 @@
+const id = 12222145
+let email = "arzoobhuker09@gmail.com"
+var password = "21212121"
+AccountName = "Arzoo"
+
+console.log(id);
+// const-> can't change
+// var -> issue with block scope and is functional scoped
+// let -> block scoped 
+console.table([id,email,password,AccountName]);
